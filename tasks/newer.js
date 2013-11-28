aa605e2a2170ef5fb2e32b793f24f0684a8cd21c
drawk <ggautier@b2pweb.com>
2013-11-28 15:53:29 +0100

    export some code in a library

--- tasks/newer.js
+++ tasks/newer.js
@@ -8,81 +8,9 @@
 
 'use strict';
 
+var outofdate = require('grunt-task-newer-lib').outofdate;
+
 module.exports = function(grunt) {
-    /**
-     * @type {Object}
-     */
-    var fs = require('fs');
-   
-    /**
-     * @param {string} file
-     * 
-     * @return {Object}
-     */
-    var getStatFile = function(file) {
-        var stat;
-
-        try {
-            stat = fs.statSync(file);
-        } catch (exception) {
-            stat = null;
-        }
-
-        return stat;
-    };
-
-    /**
-     * @param {Object} stat
-     * 
-     * @return {number}
-     */
-    var getModifiedTime = function(stat) {
-        var mtime;
-
-        try {
-            mtime = stat.mtime.getTime();
-        } catch (exception) {
-            mtime = 0;
-        }
-
-        return mtime;
-    };
-
-    /**
-     * @param {string}          target
-     * @param {Array.<string>}  files
-     * 
-     * @return {boolean}
-     */
-    var hasNewerFiles = function(target, files) {
-        var targetStat = getStatFile(target);
-
-        if (!targetStat) {
-            return true;
-        }
-
-        var targetModifiedTime = getModifiedTime(targetStat);
-
-        if (!targetModifiedTime) {
-            return true;
-        }
-
-        for (var i = 0, iCount = files.length; i < iCount; i++) {
-            var fileStat = getStatFile(files[i]);
-
-            if (fileStat === null) {
-                continue;
-            }
-
-            if (getModifiedTime(fileStat) > targetModifiedTime) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-
     grunt.registerTask('newer', 'Run grunt task only if source files are newer.', function(name, target) {
         var prefix = this.name;
 
@@ -98,12 +26,11 @@ module.exports = function(grunt) {
             return grunt.task.run(tasks);
         }
 
-        var args        = Array.prototype.slice.call(arguments, 2).join(':'),
-            config      = grunt.util._.clone(grunt.config.get([name, target])),
-            newerFiles  = grunt.task.normalizeMultiTaskFiles(config, target)
-                            .filter(function(f) {
-                                return hasNewerFiles(f.dest, f.src);
-                            });
+        var args       = Array.prototype.slice.call(arguments, 2).join(':'),
+            config     = grunt.util._.clone(grunt.config.get([name, target])),
+            newerFiles = grunt.task.normalizeMultiTaskFiles(config, target).filter(function(f) { 
+                             return outofdate(f.dest, f.src); 
+                         });
 
         if (!newerFiles.length) {
             return;
